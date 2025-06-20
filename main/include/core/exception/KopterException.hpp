c7ef52d1a602995911c9f0e5664366f1e71cd7ca
Ivan Somov <msijoc@gmail.com>
2025-06-20 18:15:36 +0300

    Removed unused macro for exception

--- main/include/core/exception/KopterException.hpp
+++ main/include/core/exception/KopterException.hpp
@@ -18,13 +18,6 @@
 
 namespace kopter {
 
-#define CHECK_THROW_WITH(error, exception_type)                                                                        \
-    do {                                                                                                               \
-        esp_err_t result = (error);                                                                                    \
-        if (result != ESP_OK)                                                                                          \
-            throw kopter::exception_type(result);                                                                      \
-    } while (0)
-
 struct KopterException : public idf::ESPException {
     KopterException(esp_err_t error) : idf::ESPException(error)
     {
--- main/include/core/exception/KopterExceptionUtils.hpp
+++ main/include/core/exception/KopterExceptionUtils.hpp
@@ -1,16 +1,44 @@
+/*
+ Copyright 2025 Ivan Somov
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 #pragma once
 
 #include "KopterException.hpp"
 
+#include <type_traits>
+
 namespace kopter {
 
-template <typename Func, class Exception> inline auto wrap_call(Func func) -> decltype(func())
+#define CHECK_THROW_WITH(error, exception_type)                                                                        \
+    do {                                                                                                               \
+        esp_err_t result = (error);                                                                                    \
+        if (result != ESP_OK)                                                                                          \
+            throw kopter::exception_type(result);                                                                      \
+    } while (0)
+
+template <typename Exception,
+          typename Func,
+          typename = std::enable_if_t<std::is_base_of<KopterException, Exception>::value>>
+inline auto check_call(Func func) -> decltype(func())
 {
     try {
         return func();
     }
     catch (const idf::ESPException &e) {
-        throw I2cException(e.error);
+        throw Exception(e.error);
     }
 }
 
