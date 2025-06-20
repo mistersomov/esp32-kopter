f650b2d9ebfb5586abc4c62c9c8d32fc3da942f7
Ivan Somov <msijoc@gmail.com>
2025-06-20 18:53:07 +0300

    Added brushed DC motor (BDC) class

---
+++ main/include/motor/bdc/BDCMotor.hpp
@@ -0,0 +1,115 @@
+/*
+ Copyright 2025 Ivan Somov
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#pragma once
+
+#include "IMotor.hpp"
+
+#include "bdc_motor.h"
+#include "soc/gpio_num.h"
+
+namespace kopter {
+
+/**
+ * @class BDCMotor
+ * @brief Represents a brushed DC (BDC) motor controlled via MCPWM on ESP-IDF.
+ *
+ * This class provides a C++ interface to control a brushed DC motor using the ESP-IDF
+ * `bdc_motor` driver. It implements the `IMotor` interface, allowing clients to
+ * enable or disable the motor, set its speed, and control its direction.
+ *
+ * Internally, it uses the MCPWM peripheral configured at a default frequency of 25 kHz
+ * with a timer resolution of 80 MHz.
+ *
+ * @note This class throws `MotorException` (or subclasses of `std::runtime_error`) if
+ * any motor driver operations fail.
+ */
+class BDCMotor : public IMotor {
+public:
+    /**
+     * @brief Ctor a BDCMotor instance.
+     *
+     * Initializes the underlying motor driver with the specified PWM GPIO pin and default
+     * PWM settings (25 kHz frequency, 80 MHz timer resolution).
+     *
+     * @param name A string identifier for the motor, used for logging/debugging.
+     * @param gpio The GPIO number connected to the motor's PWM control input.
+     *
+     * @throws MotorException if initialization of the motor driver fails.
+     */
+    BDCMotor(const std::string &name, gpio_num_t gpio);
+
+    /**
+     * @brief Dtor for BDCMotor.
+     *
+     * Releases resources associated with the motor driver.
+     *
+     * @throws MotorException if releasing the motor driver fails.
+     */
+    ~BDCMotor() override;
+
+    /**
+     * @brief Enables the motor hardware and prepares it for operation.
+     *
+     * @throws MotorException if the enable operation fails.
+     */
+    void enable() override;
+
+    /**
+     * @brief Disables the motor, stopping its operation and putting it in an inactive state.
+     *
+     * @throws MotorException if disabling the motor fails.
+     */
+    void disable() override;
+
+    /**
+     * @brief Sets the speed of the motor using a PWM duty cycle.
+     *
+     * The speed parameter controls the PWM duty cycle applied to the motor.
+     * Valid speed values range from 0 (motor stopped) to 3300 (maximum speed).
+     *
+     * @param speed Desired motor speed in the range [0..3300].
+     *
+     * @throws MotorException if setting the speed fails.
+     */
+    void set_speed(uint16_t speed) override;
+
+    /**
+     * @brief Coasts the motor, letting it spin freely without applying braking.
+     *
+     * @throws MotorException if the coast operation fails.
+     */
+    void coast();
+
+    /**
+     * @brief Sets the motor rotation direction to forward.
+     *
+     * @throws MotorException if setting direction fails.
+     */
+    void forward();
+
+    /**
+     * @brief Sets the motor rotation direction to reverse.
+     *
+     * @throws MotorException if setting direction fails.
+     */
+    void reverse();
+
+private:
+    bdc_motor_handle_t m_motor{nullptr};
+};
+
+} // namespace kopter
---
+++ main/src/motor/bdc/BDCMotor.cpp
@@ -0,0 +1,81 @@
+/*
+ Copyright 2025 Ivan Somov
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#include "pch.hpp"
+#include "BDCMotor.hpp"
+
+#include "MotorException.hpp"
+
+namespace kopter {
+
+constexpr uint8_t MCPWM_GROUP_ID = 0;
+constexpr uint32_t MCPWM_FREQ_HZ = 25000;                // 25KHz PWM
+constexpr uint32_t MCPWM_TIMER_RESOLUTION_HZ = 80000000; // 80MHz, 1 tick = 0.1us
+constexpr uint32_t MCPWM_DUTY_TICK_MAX =
+    MCPWM_TIMER_RESOLUTION_HZ / MCPWM_FREQ_HZ; // maximum value we can set for the duty cycle, in ticks
+
+BDCMotor::BDCMotor(const std::string &name, gpio_num_t gpio) : IMotor(std::move(name))
+{
+    bdc_motor_config_t motor_config{};
+    motor_config.pwm_freq_hz = MCPWM_FREQ_HZ;
+    motor_config.pwma_gpio_num = gpio;
+
+    bdc_motor_mcpwm_config_t mcpwm_config{};
+    mcpwm_config.group_id = MCPWM_GROUP_ID;
+    mcpwm_config.resolution_hz = MCPWM_TIMER_RESOLUTION_HZ;
+
+    check_call<MotorException>(
+        [&]() { CHECK_THROW(bdc_motor_new_mcpwm_device(&motor_config, &mcpwm_config, &m_motor)); });
+}
+
+BDCMotor::~BDCMotor()
+{
+    if (m_motor) {
+        check_call<MotorException>([this]() { CHECK_THROW(bdc_motor_del(m_motor)); });
+    }
+}
+
+void BDCMotor::enable()
+{
+    check_call<MotorException>([this]() { CHECK_THROW(bdc_motor_enable(m_motor)); });
+}
+
+void BDCMotor::disable()
+{
+    check_call<MotorException>([this]() { CHECK_THROW(bdc_motor_disable(m_motor)); });
+}
+
+void BDCMotor::set_speed(uint16_t speed)
+{
+    check_call<MotorException>([speed, this]() { CHECK_THROW(bdc_motor_set_speed(m_motor, speed)); });
+}
+
+void BDCMotor::coast()
+{
+    check_call<MotorException>([this]() { CHECK_THROW(bdc_motor_coast(m_motor)); });
+}
+
+void BDCMotor::forward()
+{
+    check_call<MotorException>([this]() { CHECK_THROW(bdc_motor_forward(m_motor)); });
+}
+
+void BDCMotor::reverse()
+{
+    check_call<MotorException>([this]() { CHECK_THROW(bdc_motor_reverse(m_motor)); });
+}
+
+} // namespace kopter
