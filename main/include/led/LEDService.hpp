68492f80349337c542874ef6ee81400b31006b7b
Ivan Somov <msijoc@gmail.com>
2025-07-17 18:47:24 +0300

    Added LED service

---
+++ main/include/led/LEDService.hpp
@@ -0,0 +1,129 @@
+/*
+ Copyright 2025 Ivan Somov
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#pragma once
+
+#include "LEDColor.hpp"
+#include "RGBColor.hpp"
+#include "Task.hpp"
+
+#include "led_strip.h"
+#include "soc/gpio_num.h"
+
+namespace kopter {
+
+/**
+ * @brief Singleton class to manage RGB LED strip effects and configuration.
+ *
+ * The LEDService class encapsulates LED strip setup, color parsing, and blinking behaviors.
+ * It supports both one-time blinks and continuous blinking in a separate FreeRTOS task.
+ */
+class LEDService {
+public:
+    LEDService(const LEDService &) = delete;
+    LEDService &operator=(const LEDService &) = delete;
+
+    /**
+     * @brief Dtor. Turns off the LED strip and releases resources.
+     *
+     * @throws LEDException if releasing the LED strip fails.
+     */
+    ~LEDService();
+
+    /**
+     * @brief Get the singleton instance of the LEDService.
+     * @return Reference to the LEDService singleton.
+     */
+    static LEDService &get_instance();
+
+    /**
+     * @brief Configure the LED strip with the specified GPIO and number of LEDs.
+     *
+     * This method initializes the RMT-based LED strip driver.
+     *
+     * @param gpio GPIO pin connected to the LED strip data line.
+     * @param num_leds Number of LEDs on the strip.
+     * @param led_type Type of the LED model (default: WS2812).
+     *
+     * @throws LEDException if LED strip initialization fails.
+     */
+    void configure_led(gpio_num_t gpio, size_t num_leds, led_model_t led_type = led_model_t::LED_MODEL_WS2812);
+
+    /**
+     * @brief Start blinking the LED strip continuously with the given color and duration.
+     *
+     * This launches a background FreeRTOS task that repeatedly blinks the LEDs.
+     * If a blink task is already running, this call has no effect.
+     *
+     * @param color Color to blink.
+     * @param duration Time in milliseconds for each on/off phase.
+     *
+     * @throws LEDException if any error occurs while updating the LED strip.
+     */
+    void blink(LEDColor color, uint32_t duration);
+
+    /**
+     * @brief Stop the currently running blink task (if any) and turn off the LEDs.
+     *
+     * The method attempts to stop the task gracefully. If the task doesn't exit within a timeout, it is forcibly reset.
+     *
+     * @throws LEDException if clearing the LED strip fails.
+     */
+    void stop_blink();
+
+    /**
+     * @brief Perform a single blink (on + off) with the given color and duration.
+     *
+     * @param color Color to blink.
+     * @param duration Time in milliseconds for the on and off phases.
+     *
+     * @throws LEDException if setting or clearing the LED pixels fails.
+     */
+    void blink_once(LEDColor color, uint32_t duration);
+
+    /**
+     * @brief Immediately turn off all LEDs on the strip.
+     *
+     * @throws LEDException if clearing the LED strip fails.
+     */
+    void off();
+
+private:
+    LEDService();
+
+    /**
+     * @brief Convert a high-level LEDColor enum to an RGBColor value.
+     *
+     * @param color Logical LED color.
+     * @return Corresponding RGB color.
+     */
+    RGBColor parse_color(LEDColor color) noexcept;
+
+    /**
+     * @brief Fill all LEDs on the strip with the given RGB color and refresh the display.
+     *
+     * @param color RGB color to display.
+     *
+     * @throws LEDException if updating the LED pixels or refreshing the strip fails.
+     */
+    void fill_strip(const RGBColor &color);
+
+    led_strip_handle_t m_led_strip_handler;
+    std::optional<Task> m_blink_task;
+    std::atomic<bool> m_blink_running{false};
+    size_t m_num_leds;
+};
+} // namespace kopter
---
+++ main/src/led/LEDService.cpp
@@ -0,0 +1,164 @@
+/*
+ Copyright 2025 Ivan Somov
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#include "pch.hpp"
+#include "LEDService.hpp"
+
+#include "LEDException.hpp"
+
+namespace kopter {
+
+namespace {
+constexpr uint32_t RMT_HZ_RESOLUTION = 10 * 1000 * 1000; // 10 MHz
+constexpr std::string_view BLINK_TASK_NAME = "led_blinking_task";
+constexpr uint16_t BLINK_TASK_STACK_SIZE = 4096;
+constexpr uint16_t BLINK_TASK_STOP_DELAY = 50;
+constexpr RGBColor COLORS[] = {
+    RGBColor(0, 0, 0),       // OFF
+    RGBColor(127, 127, 127), // default / white-ish
+    RGBColor(127, 0, 0),     // RED
+    RGBColor(127, 127, 0),   // YELLOW
+    RGBColor(0, 127, 0)      // GREEN
+};
+constexpr std::string_view TAG = "[LEDService]";
+} // namespace
+
+LEDService::LEDService() : m_led_strip_handler{nullptr}, m_num_leds{0}
+{
+}
+
+RGBColor LEDService::parse_color(LEDColor color) noexcept
+{
+    switch (color) {
+    case LEDColor::RED:
+        return COLORS[2];
+    case LEDColor::YELLOW:
+        return COLORS[3];
+    case LEDColor::GREEN:
+        return COLORS[4];
+    default:
+        return COLORS[1];
+    }
+}
+
+void LEDService::fill_strip(const RGBColor &color)
+{
+    for (size_t i = 0; i != m_num_leds; i++) {
+        check_call<LEDException>(
+            [this, i, &color]() { led_strip_set_pixel(m_led_strip_handler, i, color.r, color.g, color.b); });
+    }
+    check_call<LEDException>([this]() { led_strip_refresh(m_led_strip_handler); });
+}
+
+LEDService::~LEDService()
+{
+    if (m_led_strip_handler) {
+        off();
+        check_call<LEDException>([this]() { led_strip_del(m_led_strip_handler); });
+    }
+}
+
+LEDService &LEDService::get_instance()
+{
+    static LEDService instance;
+    return instance;
+}
+
+void LEDService::configure_led(gpio_num_t gpio, size_t num_leds, led_model_t led_type)
+{
+    if (m_led_strip_handler) {
+        ESP_LOGW(TAG.data(), "configure_led called more than once. Ignored.");
+        return;
+    }
+
+    m_num_leds = num_leds;
+
+    led_strip_config_t cfg{};
+    cfg.strip_gpio_num = gpio;
+    cfg.max_leds = num_leds;
+    cfg.color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_GRB;
+    cfg.led_model = led_type;
+    cfg.flags.invert_out = false;
+
+    led_strip_rmt_config_t rmt_cfg{};
+    rmt_cfg.clk_src = RMT_CLK_SRC_DEFAULT;
+    rmt_cfg.resolution_hz = RMT_HZ_RESOLUTION;
+    rmt_cfg.flags.with_dma = false;
+
+    check_call<LEDException>(
+        [this, &cfg, &rmt_cfg]() { led_strip_new_rmt_device(&cfg, &rmt_cfg, &m_led_strip_handler); });
+}
+
+void LEDService::blink(LEDColor color, uint32_t duration)
+{
+    if (m_blink_task.has_value()) {
+        return;
+    }
+
+    m_blink_running = true;
+    m_blink_task.emplace(BLINK_TASK_NAME.data(), BLINK_TASK_STACK_SIZE, [this, color, duration]() {
+        while (m_blink_running.load(std::memory_order_acquire)) {
+            blink_once(color, duration);
+        }
+    });
+}
+
+void LEDService::stop_blink()
+{
+    if (!m_blink_task.has_value()) {
+        return;
+    }
+
+    m_blink_running = false;
+
+    for (int i = 0; i < 20; ++i) {
+        if (!m_blink_task.has_value()) {
+            break;
+        }
+        vTaskDelay(pdMS_TO_TICKS(BLINK_TASK_STOP_DELAY));
+    }
+
+    if (m_blink_task.has_value()) {
+        ESP_LOGW(TAG.data(), "Blink task did not stop gracefully, resetting");
+        m_blink_task.reset();
+    }
+
+    off();
+    ESP_LOGW(TAG.data(), "Blink was stopped");
+}
+
+void LEDService::blink_once(LEDColor color, uint32_t duration)
+{
+    if (m_led_strip_handler == nullptr) {
+        return;
+    }
+
+    const auto rgb_color = parse_color(color);
+
+    fill_strip(rgb_color);
+    vTaskDelay(pdMS_TO_TICKS(duration));
+    fill_strip(COLORS[0]);
+    vTaskDelay(pdMS_TO_TICKS(duration));
+}
+
+void LEDService::off()
+{
+    if (m_led_strip_handler) {
+        check_call<LEDException>([this]() { led_strip_clear(m_led_strip_handler); });
+    }
+}
+
+} // namespace kopter
